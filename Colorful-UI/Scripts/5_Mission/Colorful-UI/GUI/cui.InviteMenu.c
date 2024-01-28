bc288265d593c7e6b804675682808d4b2e4c7a25
DayZ n' Chill <brian@dayznchill.com>
2024-01-28 13:13:00 -0600

    Added final pages to UI before Release
    Getting things wrapped up before the release.

---
+++ Colorful-UI/Scripts/5_Mission/Colorful-UI/GUI/cui.InviteMenu.c
@@ -0,0 +1,38 @@
+modded class InviteMenu extends UIScriptedMenu
+{	
+	private TextWidget		m_LogoutTimetext;
+	private TextWidget		m_Info;
+	private int m_iTime;
+    private Widget m_Separator1
+	private	Widget m_shader
+
+	override Widget Init()
+	{
+		layoutRoot = GetGame().GetWorkspace().CreateWidgets("colorful-ui/gui/layouts/cui.day_z_invite_dialog.layout");
+		
+		m_LogoutTimetext = TextWidget.Cast( layoutRoot.FindAnyWidget("logoutTimeText") );
+		m_Info = TextWidget.Cast( layoutRoot.FindAnyWidget("txtInfo") );
+		m_LogoutTimetext.SetText(m_iTime.ToString());
+		
+		// Class.CastTo(m_shader, layoutRoot.FindAnyWidget("Colorful_Shader"));
+		// m_shader.SetColor(colorScheme.ShaderColor());
+
+		layoutRoot.FindAnyWidget("toolbar_bg").Show(true);
+		RichTextWidget toolbar_b = RichTextWidget.Cast(layoutRoot.FindAnyWidget("BackIcon"));
+		toolbar_b.SetText(InputUtils.GetRichtextButtonIconFromInputAction("UAUIBack", "", EUAINPUT_DEVICE_CONTROLLER, InputUtils.ICON_SCALE_TOOLBAR));
+		
+		// player should sit down if possible
+		PlayerBase player = PlayerBase.Cast(GetGame().GetPlayer());
+		if (player && player.GetEmoteManager() && !player.IsRestrained() && !player.IsUnconscious()) 
+		{
+			player.GetEmoteManager().CreateEmoteCBFromMenu(EmoteConstants.ID_EMOTE_SITA);
+			player.GetEmoteManager().GetEmoteLauncher().SetForced(EmoteLauncher.FORCE_DIFFERENT);
+		}
+		
+		GetGame().GetCallQueue( CALL_CATEGORY_SYSTEM ).CallLater( UpdateTime, 1000, true );
+        // m_Separator1 = layoutRoot.FindAnyWidget( "SeparatorPanel" );
+        // m_Separator1.SetColor(colorScheme.SeparatorColor());
+		return layoutRoot;
+	}
+	
+}
