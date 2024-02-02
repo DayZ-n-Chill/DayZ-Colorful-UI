79eaf8de34275948847e60da0c14a7ce2a7d6ee0
DayZ n' Chill <brian@dayznchill.com>
2024-02-02 01:56:03 -0600

    Reintegrated Hints
    Made it all work. no bugs this time. *cough cough

--- Colorful-UI/Scripts/3_Game/Colorful-UI/GUI/Hints/uihintpanel.c
+++ Colorful-UI/Scripts/3_Game/Colorful-UI/GUI/Hints/uihintpanel.c
@@ -34,13 +34,20 @@ modded class UiHintPanelLoading extends UiHintPanel
 
 	override protected void BuildLayout(Widget parent_widget)
 	{
+        protected ImageWidget m_tShader;
+	    protected ImageWidget m_bShader;
+        protected ImageWidget m_tipIcon;
+
 		m_RootFrame = m_Game.GetWorkspace().CreateWidgets( m_RootPath, parent_widget );
 		
 		if (m_RootFrame)
 		{
 		    m_tipLineL          = ImageWidget.Cast(m_RootFrame.FindAnyWidget("LinesImageLeft"));
             m_tipLineR          = ImageWidget.Cast(m_RootFrame.FindAnyWidget("LinesImageRight"));
-            m_SpacerFrame		= m_RootFrame.FindAnyWidget("GridSpacerWidget1");		
+            m_SpacerFrame		= m_RootFrame.FindAnyWidget("Hints");	
+            m_tipIcon           = ImageWidget.Cast(m_RootFrame.FindAnyWidget("hintIcon"));	
+            m_tShader           = ImageWidget.Cast(m_RootFrame.FindAnyWidget("TopShader"));
+		    m_bShader           = ImageWidget.Cast(m_RootFrame.FindAnyWidget("BottomShader"));
 			m_UiLeftButton		= ButtonWidget.Cast(m_RootFrame.FindAnyWidget("LeftButton"));		
 			m_UiRightButton		= ButtonWidget.Cast(m_RootFrame.FindAnyWidget("RightButton"));
 			m_UiHeadlineLabel	= TextWidget.Cast(m_RootFrame.FindAnyWidget("HeadlineLabel"));	
@@ -48,10 +55,13 @@ modded class UiHintPanelLoading extends UiHintPanel
 			m_UiHintImage		= ImageWidget.Cast(m_RootFrame.FindAnyWidget("HintImage"));
 			m_UiPageingLabel	= TextWidget.Cast(m_RootFrame.FindAnyWidget("PageInfoLabel"));
             
-            m_UiHeadlineLabel.SetColor(colorScheme.TipHeader());
+            m_tipIcon.SetColor(colorScheme.TipIcon());
             m_tipLineL.SetColor(colorScheme.TipLine());
             m_tipLineR.SetColor(colorScheme.TipLine());
+            m_UiHeadlineLabel.SetColor(colorScheme.TipHeader());
             m_UiDescLabel.SetColor(colorScheme.TipText());
+            m_tShader.SetColor(colorScheme.TopShader());
+		    m_bShader.SetColor(colorScheme.BottomShader());
 			
             m_RootFrame.SetHandler(this);
 		}
