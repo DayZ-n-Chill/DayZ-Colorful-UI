04e07a5e916727e439b39f11e4b2974684d57c41
DayZ n' Chill <brian@dayznchill.com>
2024-05-27 17:43:49 -0500

    Fix error messages

--- Utils/Batch/Setup/WorkshopSymlink.bat
+++ Utils/Batch/Setup/WorkshopSymlink.bat
@@ -37,7 +37,7 @@ IF ERRORLEVEL 1 (
 :: Create a symlink for SYMLDIR on the P drive
 mklink /J "%SYMLDIR%" "%YOURMODDIR%" >nul 2>&1
 IF ERRORLEVEL 1 (
-    powershell -Command "Write-Host 'ERROR:' -ForegroundColor Red -NoNewline; Write-Host ' Failed to create symlink for %SYMLDIR%. Please check the directory and permissions.' -ForegroundColor Red"
+    powershell -Command "Write-Host 'ERROR:' -ForegroundColor Red -NoNewline; Write-Host ' Failed to create symlink for %SYMLDIR%. Directory is already there.' -ForegroundColor DarkYellow"
     pause
     exit /b
 ) ELSE (
