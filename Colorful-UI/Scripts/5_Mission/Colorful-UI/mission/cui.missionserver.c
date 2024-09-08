789c98bcdb087a425bd8f514062a28b74df01f78
DayZ n' Chill <brian@dayznchill.com>
2024-09-08 17:11:10 -0500

    Minor changes

--- Colorful-UI/Scripts/5_Mission/Colorful-UI/mission/cui.missionserver.c
+++ Colorful-UI/Scripts/5_Mission/Colorful-UI/mission/cui.missionserver.c
@@ -1,15 +1,11 @@
 modded class MissionServer
 {
-	// Called when server initializes
 	override void OnInit()
 	{
 		super.OnInit();
 		Print("[Colorful UI] OnInit");
-
-		// Load config
-		GetColorfulConfig();
+    
+    	GetColorfulConfig();
         Print("[Colorful UI] Loading config");
 	}
-
 }
-
